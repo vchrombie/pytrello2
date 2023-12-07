9fd78af679e726a5a3db788e652213650453732a
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-12-07 04:16:22 -0500

    feat: Implement base wrapper and Board fetching
    This commit sets up the initial file structure and
    implementations for the Trello API python wrapper.
    It includes:
    
    - Base client and utils files
    - Exception handling
    - Board fetching and models
    - Removal of old main entry point
    - Addition of new files and folders
    
    Signed-off-by: Venu Vardhan Reddy Tekula <vt2182@nyu.edu>

---
+++ pytrello2/models/board.py
@@ -0,0 +1,20 @@
+class Board:
+    """
+    Represents a Trello board.
+    """
+
+    def __init__(self, data):
+        """
+        Initializes a new Board object.
+        """
+        self.id = data.get('id')
+        self.name = data.get('name')
+        self.desc = data.get('desc')
+        self.closed = data.get('closed')
+        self.url = data.get('url')
+  
+    def __str__(self):
+        """
+        Returns a string representation of a Board object.
+        """
+        return f'Board(id={self.id}, name={self.name}, desc={self.desc}, url={self.url})'
